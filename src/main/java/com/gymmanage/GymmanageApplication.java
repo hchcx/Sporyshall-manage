6cabc0bfe02159ddd5065a91c20130ebda9bb5ba
hch <1719422449@qq.com>
2023-02-26 14:26:24 +0800

    增加异步注解

--- src/main/java/com/gymmanage/GymmanageApplication.java
+++ src/main/java/com/gymmanage/GymmanageApplication.java
@@ -2,8 +2,10 @@ package com.gymmanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class GymmanageApplication {
 
     public static void main(String[] args) {
